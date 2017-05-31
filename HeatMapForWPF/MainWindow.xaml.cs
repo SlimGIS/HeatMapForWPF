f52327dd6863c480b69a2faa931fc1c3328385e6
SlimGIS_Dev <dev@slimgis.com>
2017-05-31 23:54:36 +0800

    Init project.

---
+++ HeatMapForWPF/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using SlimGis.MapKit.Controls;
+using SlimGis.MapKit.Geometries;
+using SlimGis.MapKit.Layers;
+using SlimGis.MapKit.Symbologies;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HeatMapForWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static readonly string columnName = "MED_AGE";
+        private ColorPaletteType colorPaletteType;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ColorPaletteTypeComboBox.Items.Add(ColorPaletteType.Default);
+            ColorPaletteTypeComboBox.Items.Add(ColorPaletteType.Gray);
+            ColorPaletteTypeComboBox.Items.Add(ColorPaletteType.Rainbow);
+            ColorPaletteTypeComboBox.Items.Add(ColorPaletteType.RedWhiteBlue);
+            ColorPaletteTypeComboBox.Items.Add(ColorPaletteType.Thermal);
+        }
+
+        private void map_Loaded(object sender, RoutedEventArgs e)
+        {
+            map.MapUnit = GeoUnit.Meter;
+            map.UseOpenStreetMapAsBaseMap();
+
+            ShapefileLayer pointLayer = new ShapefileLayer("SampleData/cities-900913.shp");
+            HeatStyle heatStyle = new HeatStyle();
+            heatStyle.DataColumn = columnName;
+            heatStyle.MaxIntensity = 37.7;
+            heatStyle.MinIntensity = 30;
+            heatStyle.Alpha = 150;
+            heatStyle.Radius = (2 - HeatPointSizeComboBox.SelectedIndex) * 30 + 20;
+            heatStyle.ColorPaletteType = colorPaletteType;
+            pointLayer.Styles.Add(heatStyle);
+            map.AddStaticLayers("PointOverlay", pointLayer);
+
+            map.ZoomTo(pointLayer.GetBound());
+        }
+
+        private void HeatPointSizeComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Refresh();
+        }
+
+        private void ColorPaletteTypeComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            colorPaletteType = (ColorPaletteType)ColorPaletteTypeComboBox.SelectedItem;
+            Refresh();
+        }
+
+        private void Refresh()
+        {
+            ShapefileLayer pointLayer = map.FindLayer<ShapefileLayer>("cities-900913");
+            if (pointLayer == null) return;
+            HeatStyle heatStyle = (HeatStyle)pointLayer.Styles[0];
+            heatStyle.Radius = (2 - HeatPointSizeComboBox.SelectedIndex) * 30 + 20;
+            heatStyle.ColorPaletteType = colorPaletteType;
+
+            map.Refresh(RefreshType.Redraw);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,35 @@
+# Build Heat maps with SlimGIS MapKit WPF
+
+Heat mapping, from a  geographic perspective, is a method of showing the geographic clustering of a phenomenon.  Sometimes also referred as hot spot mapping, heat maps show locations of higher densities of geographic entities. ‘heat’ in the term refers to the concentration of the geographic entity within any given spot, not to be confused with heat mapping that refers to the mapping of actual temperatures on the earth’s surface.  Heat mapping is a way of geographically visualizing locations so that patterns of higher than average occurrence of things likes crime activity, traffic accidents, or store locations can emerge.
+
+This video represents a Heat Maps demo that is created by SlimGIS MapKit WPF control. Besides the basic heat map, we will also show you the powerful configure for the heat maps. In the demo, you will find out we switched the heat maps style which are build-in in our control. Also, you could adjust the heat radius size to see. Dive into the API, we provides a color list so that you could customize your own colors.
+
+> This project already includes the runtime license. If your trial license is expired, it still can run the project by "ctrl + F5". Or [try executable](https://github.com/SlimGIS/HeatMapForWPF/releases). 
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/QqFdwrOk5m0" frameborder="0" allowfullscreen></iframe>
+
+[Watch on YouTube](https://youtu.be/QqFdwrOk5m0)
+
+Check out the source code, you will find our Heat Maps is formed a vector data source with heat values and a HeatStyle. 
+
+We have 4 build-in color patterns and a custom switcher to allow to use custom color pattern.
+
+- Themal
+
+![HeatMaps-Themal](https://github.com/SlimGIS/HeatMapForWPF/blob/master/Preview/preview-heatmap-themal.PNG?raw=true)
+
+- Rainbow
+
+![HeatMaps-Rainbow](https://github.com/SlimGIS/HeatMapForWPF/blob/master/Preview/preview-heatmap-default.PNG?raw=true)
+
+- RedWhiteBlue
+
+![HeatMaps-RWB](https://github.com/SlimGIS/HeatMapForWPF/blob/master/Preview/preview-heatmap-red-white-blue.PNG?raw=true)
+
+- Gray
+
+![HeatMaps-Gray](https://github.com/SlimGIS/HeatMapForWPF/blob/master/Preview/preview-heatmap-gray.PNG?raw=true)
+
+- Custom
+
+For more features, visit us at http://www.slimgis.com.
